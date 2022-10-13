dbff284a3e57d57c8f8287cdc376d18d6c927500
ugasyare78 <ugasyare78@gmail.com>
2022-10-13 21:15:40 +0000

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
-
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	/**
-	 * main - Entry point
-	 *
-	 * Return: Always 0 (Success)
-	 */
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	char strng[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(strng);
-
-	return (0);
-
+	write(2, str1, 59);
+	return (1);
 }
